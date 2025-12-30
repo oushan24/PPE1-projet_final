73fe498d46747f46d7ebaa2eb4d8a52c2e6c8f09
Tyllane Cazenave <tyllanecazenave@MacBook-Pro-de-Tyllane.local>
2025-12-30 18:18:40 +0100

    modif script ajout spécificité + autres modif

--- programmes/Fr.sh
+++ programmes/Fr.sh
@@ -39,6 +39,7 @@ cat > "$SORTIE" <<EOF
                 <th>Dump textuel</th>
                 <th>Contexte</th>
                 <th>Concordancier</th>
+                <th>Spécificité</th>
 
             </tr>
         </thead>
@@ -76,16 +77,28 @@ while read -r LINE ; do
 
         # Extraire le contenu textuel des pages 
         FICHIER_DUMP=$"../dump/lang_fr-${NB_LIGNE}.txt"
-        lynx -dump -nolist "$FICHIER_ASP" > "$FICHIER_DUMP"
+        lynx -dump -nolist "$FICHIER_ASP" | iconv -f ISO-8859-1 -t UTF-8 | sponge "$FICHIER_DUMP" # Conversion en UTF-8 parce que dans mon ordi, les fichiers ne s'ouvres pas correctement (problèmes d'accent etc) au début, j'avais mis la conversion au niveau des fichiers contextes mais j'ai fini par la mettre dès le dump parce que j'ai trouvé des problèmes d'encodages aussi ici, et comme ça le reste n'a pas de problèmes puisque je m'appuie du dump pour faire le contexte
         DUMP=$"<a href='$FICHIER_DUMP' target='_blank'>Texte n°$NB_LIGNE</a>"
 
         # Compte du nombre du mot cible dans la page
-        NB_OCCURENCES=$(grep -oEi "[a-zA-Z]*?charge[s]?" "$FICHIER_DUMP" | wc -l)
+        NB_OCCURENCES=$(grep -oEi "charge[s]?" "$FICHIER_DUMP" | wc -l)
 
         # Contexte (quelques lignes autour du mot cible, "zoomer" dans le dump textuel)
         FICHIER_CONTX=$"../contextes/lang_fr-${NB_LIGNE}.txt"
-        grep -i -E -C 3 "charge[s]?" "$FICHIER_DUMP" | iconv -f ISO-8859-1 -t UTF-8 | sponge "$FICHIER_CONTX" # Conversion en UTF-8 parce que dans mon ordi, les fichiers ne s'ouvres pas correctement (problèmes d'accent etc)
+        grep -i -E -C 3 "charge[s]?" "$FICHIER_DUMP" >> "$FICHIER_CONTX"
         CONTEXTE=$"<a href='$FICHIER_CONTX' target='_blank'>Contexte n°$NB_LIGNE</a>"
+
+         #Spécificité (textométrie)
+        FICHIER_TEMP_TOK="temp_tokens.txt" # Le script coocurents.py attend 1 mot par ligne donc on peut pas directement mettre les fichiers dump comme ça
+        FICHIER_SPE=$"../specificite/specificite_fr-${NB_LIGNE}.tsv"
+        
+        grep -oEi "[a-zA-Záàâäãåçéèêëíìîïñóòôöõúùûüýÿæœ]+" "$FICHIER_DUMP" > "$FICHIER_TEMP_TOK" # Transformation des dumps en listes de mots
+
+        python3 ../specificite/cooccurrents.py --target "charges?" "$FICHIER_TEMP_TOK" -N 10 -s i --match-mode regex > "$FICHIER_SPE"
+       
+        rm "$FICHIER_TEMP_TOK"
+
+        SPECIFICITE=$"<a href='$FICHIER_SPE' target='_blank'>Spécificité n°$NB_LIGNE</a>"
 
         # Concordancier (créer un fichier pour chaque URL à partir du contexte précédemment produit, construction de tableaux à mettre dans un dossier "Concordancier" et mettre les liens de chaque concordancier dans le tableau de base)
         FICHIER_CONCO=$"../concordanciers/lang_fr-${NB_LIGNE}.html"
@@ -125,7 +138,7 @@ EOF
         CONCORDANCIER=$"<a href='$FICHIER_CONCO' target='_blank'>Concordancier n°$NB_LIGNE</a>"
 
         # Tableau final avec toutes les infos
-        echo "<tr><td>$NB_LIGNE</td><td id="url">$LINE</td><td>$CODE</td><td>$ENCODAGE_OU_PAS</td><td>$NB_MOTS</td><td>$NB_OCCURENCES</td><td>$ASPIRATION</td><td>$DUMP</td><td>$CONTEXTE</td><td>$CONCORDANCIER</td></tr>" >> "$SORTIE"
+        echo "<tr><td>$NB_LIGNE</td><td id="url">$LINE</td><td>$CODE</td><td>$ENCODAGE_OU_PAS</td><td>$NB_MOTS</td><td>$NB_OCCURENCES</td><td>$ASPIRATION</td><td>$DUMP</td><td>$CONTEXTE</td><td>$CONCORDANCIER</td><td>$SPECIFICITE</td></tr>" >> "$SORTIE"
 
 fi
 done  < "$FICHIER_URLS"
